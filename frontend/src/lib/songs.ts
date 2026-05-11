56920166ee47aa271b9d67d4db238e89ec53ad0c
FusionAtom360 <joe.yakligian@gmail.com>
2026-05-10 19:07:29 -0700

    localhost accidentally included

--- frontend/src/lib/songs.ts
+++ frontend/src/lib/songs.ts
@@ -46,7 +46,7 @@ function getApiBaseUrl(): string {
         return 'http://localhost:3000';
     }
 
-    return 'http://localhost:3000';//'https://practice.josephyakligian.com';
+    return 'https://practice.josephyakligian.com';
 }
 
 export function createApiUrl(path: string): string {
